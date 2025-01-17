cf255f04926d1cec2c47ca0685551ac490fc3360
Caio Chiabai <caiochiabaioliveira@gmail.com>
2025-01-17 09:09:01 -0300

    Atualizacao das migrations

--- barbearia/src/main/resources/db/migration/V4__Populate_Table_Procedimento.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `procedimento` (`id`, `nome`, `preco`, `duracao`) VALUES
-    (1, 'Corte de cabelo', 35, '00:30:00.000000'),
-    (2, 'Barba', 30, '00:30:00.000000'),
-    (3, 'Sobrancelha', 15, '00:30:00.000000'),
-    (4, 'Alisamento', 100, '00:30:00.000000');
-
